c4c6d5e1175f6f41be8435864e6669387d67b812
Ignacio Morinigo <morinacho@gmail.com>
2022-01-05 21:57:00 -0300

    Database config

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+const {MongoClient} = require('mongodb')
+const {Config}      = require('../config/index')
+const debug         = require('debug')('app:database')
+
+
+let connection = null
+module.exports.Database = (colletion) => new Promise( async (resolve, reject) => {
+    try {
+        if(!connection){
+            const client = new MongoClient(Config.mongoUri)
+            let connection = await client.connect()
+            debug('New connecttion: MongoDB Atlas')
+        }
+        debug("Reusing connection")
+        const db = connection.db(Config.mongoDbname)
+        resolve(db.colletion(colletion))
+    } catch (error) {
+        reject(error)
+    }
+})
